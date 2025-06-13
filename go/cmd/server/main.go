8c831cb969ed43eb52a4917d91437dc6b44ab175
Raja Soundaramourty <raja.aiml@icloud.com>
2025-06-12 22:45:27 -0400

    add observability library

--- go/cmd/server/main.go
+++ go/cmd/server/main.go
@@ -2,15 +2,25 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
+	"os"
 
 	"github.com/raja-aiml/sematic-cache/go/core"
+	"github.com/raja-aiml/sematic-cache/go/observability"
 	"github.com/raja-aiml/sematic-cache/go/server"
 )
 
 func main() {
 	addr := flag.String("address", ":8080", "server address")
 	flag.Parse()
+
+	shutdown, err := observability.Init(context.Background(), "cache-server", os.Getenv("JAEGER_ENDPOINT"))
+	if err != nil {
+		log.Fatalf("otel init: %v", err)
+	}
+	defer shutdown(context.Background())
 
 	cache := core.NewCache(100)
 	srv := server.New(cache)
--- go/go.mod
+++ go/go.mod
@@ -3,5 +3,7 @@ module github.com/raja-aiml/sematic-cache/go
 go 1.23.8
 
 require (
+    go.opentelemetry.io/otel v1.17.0
+    go.opentelemetry.io/otel/exporters/jaeger v1.17.0
 )
 
